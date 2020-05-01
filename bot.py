5c8cc8b381ae6768f633436520651778e8ae9c16
Cemre Süler <Cemre8@live.nl>
2020-05-01 22:12:47 +0200

    Removal of messages when reacting with an X
    Changes:
    ! Better Command
    + Auto setup
    + Auto reactions
    + Removal when reacting with an X emoji

--- bot.py
+++ bot.py
@@ -1,28 +1,35 @@
-# bot.py
+#bot.py
+#loading in the dependencies
 import os
-
 import discord
 from discord.utils import get
 from dotenv import load_dotenv
+import createChannels
 
-load_dotenv()
+#NEED TO FIX THIS WITH A BETTER ENV FILE!!
 TOKEN = 'NzA1Nzg0NDU0NzI3MzM1OTM2.Xqw56w.17hlBidfGbewgCyWOUMV4nc2mEE'
 GUILD = '705112198585778186'
 client = discord.Client()
 
 @client.event
+#Tells to the console the bot is ready
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    
 
 @client.event
+#Calls this event whenever a message was sent to the server
 async def on_message(message):
-    rightChannel = discord.get_channel("705825738850435072")
-    if message.author == client.user:
-        return
-    if len(message.content) > 0:
-        if (message.channel == rightChannel):
-            await message.channel.send("Er zou nu een emoji moeten worden toegevoegd.")
-            await message.add_reaction("\U0001F680")
-            await message.add_reaction("\U0000274C")
+    await createChannels.createChannels(message)
+    if(message.channel.name == "to-do"):
+        await message.add_reaction("\U0001F680")
+        await message.add_reaction("\U0000274C")
 
+@client.event
+async def on_reaction_add(reaction,user):
+    if(reaction.count == 2):
+        if(reaction.emoji == "\U0000274C"):
+            await reaction.message.delete()
+
+    
 client.run(TOKEN)
---
+++ createChannels.py
@@ -0,0 +1,43 @@
+import os
+import discord
+from discord.utils import get
+from dotenv import load_dotenv
+
+client = discord.Client()
+
+async def createChannels(message):
+    #Checks if it was a human that sent the message
+    if message.author == client.user:
+        return
+    #Setup command that you can run everywhere
+    if message.content == "td!setup":
+        #Checks if there is a tasks category and creates it if there isn't
+        if (discord.utils.get(message.guild.categories, name="tasks")):
+            await message.channel.send("There is already a category with the name 'tasks', so I won't create a new one")
+        else:
+            await message.channel.send("There is no category with the name 'tasks', I'll create one for you now!")
+            await message.guild.create_category_channel('tasks')
+
+        #Gets the ID of the 'tasks' category so it can get passed through to the text-channel creator
+        categoryID = discord.utils.get(message.guild.categories, name="tasks")
+
+        #Checks if there is a to-do channel and creates it if there isn't
+        if (discord.utils.get(message.guild.text_channels, name="to-do")):
+            await message.channel.send("There is already a text-channel with the name 'to-do', so I won't create a new one")
+        else:
+            await message.channel.send("There is no text-channel with the name 'to-do', I'll create one for you now!")
+            await message.guild.create_text_channel('to-do',category=categoryID)
+        
+        #Checks if there is an in-progress channel and creates it if there isn't
+        if (discord.utils.get(message.guild.text_channels, name="in-progress")):
+            await message.channel.send("There is already a text-channel with the name 'in-progress', so I won't create a new one")
+        else:
+            await message.channel.send("There is no text-channel with the name 'in-progress', I'll create one for you now!")
+            await message.guild.create_text_channel('in-progress',category=categoryID)
+        
+        #Checks if there is a finished channel and creates it if there isn't
+        if (discord.utils.get(message.guild.text_channels, name="finished")):
+            await message.channel.send("There is already a text-channel with the name 'finished', so I won't create a new one")
+        else:
+            await message.channel.send("There is no text-channel with the name 'finished', I'll create one for you now!")
+            await message.guild.create_text_channel('finished',category=categoryID)
